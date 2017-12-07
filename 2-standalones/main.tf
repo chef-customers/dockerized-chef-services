c9bd90be6cd0926ffe0b8fb171e46dac085e7018
Irving Popovetsky <irving@chef.io>
2017-12-06 17:39:10 -0800

    Crude end-to-end testing framework with terraform
    Signed-off-by: Irving Popovetsky <irving@chef.io>

---
+++ 2-standalones/main.tf
@@ -0,0 +1,108 @@
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+variable "aws_region" { default = "us-west-2" }
+variable "aws_profile" { default = "default" }
+variable "aws_vpc" { default = "vpc-41d45124" }
+variable "aws_subnet" { default = "subnet-7424b611" }
+variable "chef_server_instance_type" { default = "m5.xlarge" }
+variable "automate_server_instance_type" { default = "m5.2xlarge" }
+variable "aws_ami_user" { default = "centos" }
+variable "aws_key_pair_name" { default = "irving" }
+variable "aws_key_pair_file" { default = "~/.ssh/id_rsa" }
+variable "tag_dept" { default = "SCE" }
+variable "tag_contact" { default = "irving" }
+
+provider "aws" {
+  region  = "${var.aws_region}"
+  profile = "${var.aws_profile}" // uses ~/.aws/credentials by default
+}
+
+resource "random_id" "cluster_id" { byte_length = 4 }
+
+data "aws_ami" "centos" {
+  most_recent = true
+  owners = ["446539779517"]
+
+  filter {
+    name   = "name"
+    values = ["chef-highperf-centos7-*"]
+  }
+}
+
+resource "aws_instance" "automate_server" {
+  connection {
+    user        = "${var.aws_ami_user}"
+    private_key = "${file("${var.aws_key_pair_file}")}"
+  }
+
+  ami                         = "${data.aws_ami.centos.id}"
+  instance_type               = "${var.automate_server_instance_type}"
+  key_name                    = "${var.aws_key_pair_name}"
+  subnet_id                   = "${var.aws_subnet}"
+  associate_public_ip_address = true
+  ebs_optimized               = true
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 100
+    volume_type           = "gp2"
+  }
+
+  tags {
+    Name      = "dockerized-automate-server-${random_id.cluster_id.hex}"
+    X-Dept    = "${var.tag_dept}"
+    X-Contact = "${var.tag_contact}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 8080",
+      "sudo iptables -A PREROUTING -t nat -p tcp --dport 443 -j REDIRECT --to-port 8443",
+      "curl -LO https://raw.githubusercontent.com/chef-customers/dockerized-chef-services/master/2-standalones/automate.yml",
+      "export ENTERPRISE=dockerize",
+      "export ADMIN_PASSWORD=ThisIsFun",
+      "export AUTOMATE_TOKEN=93a49a4f2482c64126f7b6015e6b0f30284287ee4054ff8807fb63d9cbd1c506",
+      "sudo -E /usr/local/bin/docker-compose -f automate.yml up -d"
+    ]
+  }
+}
+
+resource "aws_instance" "chef_server" {
+  connection {
+    user        = "${var.aws_ami_user}"
+    private_key = "${file("${var.aws_key_pair_file}")}"
+  }
+
+  ami                         = "${data.aws_ami.centos.id}"
+  instance_type               = "${var.chef_server_instance_type}"
+  key_name                    = "${var.aws_key_pair_name}"
+  subnet_id                   = "${var.aws_subnet}"
+  associate_public_ip_address = true
+  ebs_optimized               = true
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 30
+    volume_type           = "gp2"
+  }
+
+  tags {
+    Name      = "dockerized-chef-server-${random_id.cluster_id.hex}"
+    X-Dept    = "${var.tag_dept}"
+    X-Contact = "${var.tag_contact}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-port 8080",
+      "sudo iptables -A PREROUTING -t nat -p tcp --dport 443 -j REDIRECT --to-port 8443",
+      "curl -LO https://raw.githubusercontent.com/chef-customers/dockerized-chef-services/master/2-standalones/chef-server.yml",
+      "export AUTOMATE_ENABLED=true",
+      "export AUTOMATE_SERVER=${aws_instance.automate_server.private_ip}",
+      "export AUTOMATE_TOKEN=93a49a4f2482c64126f7b6015e6b0f30284287ee4054ff8807fb63d9cbd1c506",
+      "sudo -E /usr/local/bin/docker-compose -f chef-server.yml up -d"
+    ]
+  }
+}
